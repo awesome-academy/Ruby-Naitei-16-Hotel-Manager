52740d94df3f6794ccd514a0c16b345eee2b8ffb
tatuan19 <trananhtuan12a10@gmail.com>
2021-08-16 09:09:57 +0700

    Login user

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
     root "static_pages#home"
     get "/home", to: "static_pages#home"
     get "/help", to: "static_pages#help"
+
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
   end
 end
